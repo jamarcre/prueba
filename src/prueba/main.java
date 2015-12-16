676f23b6739189de0cc16011c85e5b39ff89c6ba
jmartinez <PROS@158.42.187.4>
2015-12-16 17:19:30 +0100

    He añadido una fucnión!

--- src/prueba/main.java
+++ src/prueba/main.java
@@ -4,8 +4,15 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola");
-
+		System.out.println("Hola");		
+		}
+	
+	public int suma (int a) {
+		int sum = a+a;
+		return sum;
 	}
+		
+	}
+	
 
-}
+
